76a4e02fa2c98defbcec52c3b71937ae68fcbdb3
szymonborkowski <szymonborkowski10@gmail.com>
2026-07-21 21:24:25 +0100

    initial database code

--- app/database.py
+++ app/database.py
@@ -4,3 +4,20 @@ This file will be for databse connection logic.
 - Define a connection method
 - Define database initialisation
 """
+
+import mysql.connector
+
+# Docker is hosting a MySQL database - create method to connect:
+
+config = {
+  'user': 'root',
+  'password': 'example',
+  'host': '127.0.0.1',
+  'database': 'employees',  # what to insert here if db not yet created?
+  'raise_on_warnings': True
+}
+
+cnx = mysql.connector.connect(**config)
+
+cnx.close()
+
