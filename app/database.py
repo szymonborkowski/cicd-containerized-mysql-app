010a14c8f8d2c684d007f4adddf723bacdde2c73
szymonborkowski <szymonborkowski10@gmail.com>
2026-08-08 15:50:35 +0100

    running database on localhost and exposing ports for local dev, made password path independent of which dir python is running

--- app/database.py
+++ app/database.py
@@ -1,15 +1,19 @@
 """
-This file will be for databse connection logic.
+This file will be used for database connection logic.
 - Read database config from environment variables
 - Define a connection method
 - Define database initialisation
 """
 
 import mysql.connector
+import os
 
 
-# Until I create a python app container use local txt file for accessing db:
-with open("../secrets/db_password.txt") as password_file:
+# Until I create a python app container use local txt file for accessing db (once this app is containerised use the secret):
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))  # Ensures this script can be run from any directory
+password_path = os.path.join(BASE_PATH, "..", "secrets", "db_password.txt")
+
+with open(password_path) as password_file:
   password = password_file.read()
 
 
@@ -17,8 +21,9 @@ with open("../secrets/db_password.txt") as password_file:
 config = {
   'user': 'root',
   'password': password,
-  'host': '172.18.0.2',
-  'database': 'employees',  # what to insert here if db not yet created?
+  'host': '127.0.0.1',  # Temporarily use localhost, when containerised use the service name 'db' (Docker has a built-in DNS server)
+  'port': 3306,
+  'database': 'user_details',  # Tells MySQL to create a database (schema) with this name
   'raise_on_warnings': True
 }
 
