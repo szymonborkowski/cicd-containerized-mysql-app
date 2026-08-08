3c4cfad9db167bb384b2ff4284bd0af6db41a6e6
szymonborkowski <szymonborkowski10@gmail.com>
2026-08-08 16:36:43 +0100

    Adding database start-up schema for dev, and updating docker compose with start-up script

---
+++ database/initial-schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS users (
+    id            INT          AUTO_INCREMENT PRIMARY KEY,
+    username      VARCHAR(50)  NOT NULL UNIQUE,
+    email         VARCHAR(100) NOT NULL UNIQUE,
+    date_of_birth DATE         NOT NULL
+);
